32809a2b5f7e100698eee7192558fa4a1d4b0348
forbise.eu <153087360+allstudios-for@users.noreply.github.com>
2023-12-06 19:08:26 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  
+
+    <link rel="apple-touch-icon" type="image/png" href="https://cpwebassets.codepen.io/assets/favicon/apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png" />
+
+    <meta name="apple-mobile-web-app-title" content="CodePen">
+
+    <link rel="shortcut icon" type="image/x-icon" href="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico" />
+
+    <link rel="mask-icon" type="image/x-icon" href="https://cpwebassets.codepen.io/assets/favicon/logo-pin-b4b4269c16397ad2f0f7a01bcdf513a1994f4c94b8af2f191c09eb0d601762b1.svg" color="#111" />
+
+
+
+  
+  
+
+  <title>CodePen - Coming Soon Text</title>
+
+    <link rel="canonical" href="https://codepen.io/ntmxsxzv-the-lessful/pen/wvNvxgM">
+  
+  
+  
+  
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap');
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color:  #151719;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  text-align:center;
+}
+.waviy {
+  position: relative;
+  -webkit-box-reflect: below -20px linear-gradient(transparent, rgba(0,0,0,.2));
+  font-size: 60px;
+}
+.waviy span {
+  font-family: 'Alfa Slab One', cursive;
+  position: relative;
+  display: inline-block;
+  color: #fff;
+  text-transform: uppercase;
+  animation: waviy 2.5s infinite;
+  animation-delay: calc(.2s * var(--i));
+  
+}
+@keyframes waviy {
+  0%,40%,100% {
+    transform: translateY(0)
+  }
+  20% {
+    transform: translateY(-20px)
+  }
+}
+</style>
+
+  <script>
+  window.console = window.console || function(t) {};
+</script>
+
+  
+  
+</head>
+
+<body translate="no">
+  <body>
+ <div class="waviy">
+   <span style="--i:1">c</span>
+   <span style="--i:2">o</span>
+   <span style="--i:3">m</span>
+   <span style="--i:4">i</span>
+   <span style="--i:5">n</span>
+   <span style="--i:6">g</span>
+   <span style="--i:7">s</span>
+   <span style="--i:8">o</span>
+   <span style="--i:9">o</span>
+   <span style="--i:10">n</span>
+  </div>
+</body>
+  
+  
+  
+</body>
+
+</html>
